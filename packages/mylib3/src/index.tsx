cd9aec3bc948a4d556cd13ea3892c08ed52d4d29
Izik Lisbon <izikl@live.com>
2021-02-02 17:51:29 -0800

    Update mylib3 with conventional commits
    feat:  Update mylib3 with conventional commits

--- packages/mylib3/src/index.tsx
+++ packages/mylib3/src/index.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 
 // Delete me
 export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries2</div>;
+  return <div>the snozzberries taste like snozzberries3</div>;
 };
