d51a95aced2a2e52ca05f99ff5c3bec641a99d7d
Izik Lisbon <izikl@live.com>
2021-03-05 12:42:45 -0800

    no prefix

--- packages/mylib3/src/index.tsx
+++ packages/mylib3/src/index.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 
 // Delete me
 export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries7</div>;
+  return <div>the snozzberries taste like snozzberries8</div>;
 };
