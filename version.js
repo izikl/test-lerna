eec71f7dfbae08f6ee11d8a06e6a7950b8946dae
Izik Lisbon <izikl@live.com>
2021-03-11 09:22:25 -0800

    fix: myLib2 - 30

--- version.js
+++ version.js
@@ -6,4 +6,9 @@ console.log(version);
 
 fs.writeFileSync('./buildver.txt', version);
 
-
+// replace AAA=<old version> with AAA=<new version> in .env.production
+const versionEnvVariableName = "(AAA=)";
+const envFileContent = fs.readFileSync('./env.productions', 'utf8') 
+const regEx = new RegExp(`${versionEnvVariableName}=.*`, "gm")
+const updatedEnvProd = regEx.replace(envFileContent, `${versionEnvVariableName}=${version}`);
+fs.writeFileSync("./env.productions", updatedEnvProd, 'utf8');
