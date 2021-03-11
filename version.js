3c6ea5ad436c8d9da5cdf19447a99cafa4d5dbeb
Izik Lisbon <izikl@live.com>
2021-03-11 09:24:13 -0800

    chore: fix prod file name

--- version.js
+++ version.js
@@ -8,7 +8,7 @@ fs.writeFileSync('./buildver.txt', version);
 
 // replace AAA=<old version> with AAA=<new version> in .env.production
 const versionEnvVariableName = "(AAA=)";
-const envFileContent = fs.readFileSync('./env.productions', 'utf8') 
+const envFileContent = fs.readFileSync('./env.production', 'utf8') 
 const regEx = new RegExp(`${versionEnvVariableName}=.*`, "gm")
 const updatedEnvProd = regEx.replace(envFileContent, `${versionEnvVariableName}=${version}`);
 fs.writeFileSync("./env.productions", updatedEnvProd, 'utf8');
