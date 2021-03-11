ab4962b1b8c7ed645cba2c8b307fbccbf54cd108
Izik Lisbon <izikl@live.com>
2021-03-11 09:27:02 -0800

    chore: fix replace

--- version.js
+++ version.js
@@ -11,5 +11,5 @@ const fileName = './.env.production';
 const versionEnvVariableName = "(AAA=)";
 const envFileContent = fs.readFileSync(fileName, 'utf8') 
 const regEx = new RegExp(`${versionEnvVariableName}=.*`, "gm")
-const updatedEnvProd = regEx.replace(envFileContent, `${versionEnvVariableName}=${version}`);
+const updatedEnvProd = envFileContent.replace(regEx, `${versionEnvVariableName}=${version}`);
 fs.writeFileSync(fileName, updatedEnvProd, 'utf8');
